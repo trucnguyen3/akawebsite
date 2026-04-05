24f1ab4f9dfd419bc201199583116987dc6bc93e
Tr14 <tr14xt@gmail.com>
2026-04-05 11:49:08 +0700

    open port

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const port = 6595;
+const host = '0.0.0.0'; // Bắt buộc phải là 0.0.0.0 để nhận traffic từ internet
+
 app.use(express.static('public')); // Phục vụ các file tĩnh trong thư mục public
 
-app.listen(6595, () => console.log('Server chạy tại http://localhost:6595'));
+app.listen(port, host, () => {
+    console.log(`Server is running at http://${host}:${port}`);
+});
