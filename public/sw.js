497793aacc5a084aa3fe67a561f1a564bf3b2852
Tr14 <tr14xt@gmail.com>
2026-04-05 15:01:28 +0700

    add Netcore SDK

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: 'AIzaSyDAlWeaejssQz78BqhVxmJapSs7Axgz5xQ',
+    messagingSenderId: '822761089009',
+    appId: '1:822761089009:web:6ff85289f216e0dde10650',
+    projectId: 'akastore-project',
+    user_key: 'ADGMOT35CHFLVDHBJNIG50K96BSUH1PDDQFBSPLEV97HGG26AH0G',
+    siteid: '183e1413a1409529a3359628eac41949'
+};
+importScripts('//cdnt.netcoresmartech.com/swv4.js');
