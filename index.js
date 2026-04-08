a2ce2e79b899c77b0c86f15ff4a26125940c0f15
Tr14 <xuantrucx111@gmail.com>
2026-04-08 10:32:47 +0700

    add download page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 6595;
 const host = '0.0.0.0'; // Bắt buộc phải là 0.0.0.0 để nhận traffic từ internet
