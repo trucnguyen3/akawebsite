2c1a40d22b5da0bfc713a3660f54b2f36841aa5d
Tr14 <xuantrucx111@gmail.com>
2026-09-16 09:51:18 +0700

    update AppsFlyer webhook

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.post('/webhook', (req, res) => {
 app.post('/webhook-appsflyer', (req, res) => {
     console.log(`[AppsFlyer] IP: ${req.ip}`);
     console.log(`[AppsFlyer] Headers:`, req.headers);
-    console.log(`[AppsFlyer] Request:`, req);
+    console.log(`[AppsFlyer] Request:`, req.body);
     console.log(`[AppsFlyer] Response:`, res);
 
     // Lấy token từ header "authorization" do AppsFlyer gửi sang
